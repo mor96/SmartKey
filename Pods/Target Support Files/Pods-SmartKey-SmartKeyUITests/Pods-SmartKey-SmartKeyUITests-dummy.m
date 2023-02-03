0bd1ec7e649a744a8a1c20152c4625405c3bea7c
mor96 <60419965+mor96@users.noreply.github.com>
2023-02-03 12:50:52 +0200

    Delete Pods directory

--- Pods/Target Support Files/Pods-SmartKey-SmartKeyUITests/Pods-SmartKey-SmartKeyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SmartKey_SmartKeyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SmartKey_SmartKeyUITests
-@end
--- Pods/Target Support Files/Pods-SmartKey/Pods-SmartKey-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SmartKey : NSObject
-@end
-@implementation PodsDummy_Pods_SmartKey
-@end
